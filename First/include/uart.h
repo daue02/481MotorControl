44b787af8a294481ca3d92b01ccad7bdb8b877fa
logan-hartford <91397886+logan-hartford@users.noreply.github.com>
2024-11-07 16:57:31 -0500

    added notes

--- First/include/uart.h
+++ First/include/uart.h
@@ -16,4 +16,16 @@ void UART_Init();
 int receiveMessage(CommandData *cmdData);
 void motorOperationCompleteCallback(uint8_t axis, uint16_t position);
 
+/*
+    Notes about using this module:
+        - It is assumed that the Pi will send a command and then
+            wait for an acknowledgment, and a replay command before
+            sending another command.
+        - The Nucleo will not send commands without receiving a command
+            first.
+        - motorOperationCompleteCallback should be called by the motor
+            HAL when the motor operation is complete which will send a
+            completion message back to the Pi.
+ */
+
 #endif
