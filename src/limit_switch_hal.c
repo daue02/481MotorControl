e37664812985c79a85a6d2935511fd7f9b7478dc
daue02 <e2dau@uwaterloo.ca>
2025-03-12 19:21:29 -0400

    New scheme to catch fast, real triggers

--- src/limit_switch_hal.c
+++ src/limit_switch_hal.c
@@ -7,7 +7,7 @@
 void EXTI9_5_IRQHandler(void);
 void Switch_Init(InterruptSwitch *interruptSW);
 
-#define LIMIT_SWITCH_DEBOUNCE_MS 10  // Software debounce time in milliseconds
+#define LIMIT_SWITCH_DEBOUNCE_MS 20 // Tuned software debounce time
 #define DEBOUNCE_OK(last_time, now) ((now - last_time) > LIMIT_SWITCH_DEBOUNCE_MS)
 
 InterruptSwitch ySW_pos =
@@ -15,32 +15,28 @@ InterruptSwitch ySW_pos =
         .name = "ySwitchPos",
         .port = GPIOB,
         .pin = GPIO_PIN_9,
-        .lastDebounceTime = 0
-};
+        .lastDebounceTime = 0};
 
 InterruptSwitch ySW_neg =
     {
         .name = "ySwitchNeg",
         .port = GPIOB,
         .pin = GPIO_PIN_8,
-        .lastDebounceTime = 0
-};
+        .lastDebounceTime = 0};
 
 InterruptSwitch zSW_pos =
     {
         .name = "zSwitchPos",
         .port = GPIOB,
         .pin = GPIO_PIN_6,
-        .lastDebounceTime = 0
-};
+        .lastDebounceTime = 0};
 
 InterruptSwitch zSW_neg =
     {
         .name = "zSwitchNeg",
         .port = GPIOB,
         .pin = GPIO_PIN_5,
-        .lastDebounceTime = 0
-};
+        .lastDebounceTime = 0};
 
 /**
  * @brief Initializes the pins and state of the limit switch / pi interrupt.
@@ -70,7 +66,6 @@ void Limit_Switch_Init(void)
     Switch_Init(&zSW_pos);
     Switch_Init(&zSW_neg);
 
-    // Enable and set EXTI line Interrupt to the given priority
     HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 }
@@ -90,74 +85,98 @@ void EXTI9_5_IRQHandler(void)
         return;
     }
 
-    uint32_t now = HAL_GetTick();  // Get current system time
+    uint32_t now = HAL_GetTick();
 
     GPIO_PinState yPin_p_state = HAL_GPIO_ReadPin(ySW_pos.port, ySW_pos.pin);
     GPIO_PinState yPin_n_state = HAL_GPIO_ReadPin(ySW_neg.port, ySW_neg.pin);
     GPIO_PinState zPin_p_state = HAL_GPIO_ReadPin(zSW_pos.port, zSW_pos.pin);
     GPIO_PinState zPin_n_state = HAL_GPIO_ReadPin(zSW_neg.port, zSW_neg.pin);
 
-    if (ySW_pos.Pin_state != yPin_p_state && DEBOUNCE_OK(ySW_pos.lastDebounceTime, now))
+    // Y+
+    if (ySW_pos.Pin_state != yPin_p_state)
     {
-        ySW_pos.lastDebounceTime = now;
-        if (yPin_p_state)
+        HAL_GPIO_EXTI_IRQHandler(ySW_pos.pin); // Always clear IRQ
+
+        if (DEBOUNCE_OK(ySW_pos.lastDebounceTime, now))
         {
-            motorY.isMoving = 0;
-            LOG_INFO("Y+ Engaged");
+            ySW_pos.lastDebounceTime = now;
+            if (yPin_p_state)
+            {
+                motorY.isMoving = 0;
+                LOG_INFO("Y+ Engaged");
+            }
+            else
+            {
+                LOG_INFO("Y+ Disengaged");
+            }
         }
-        else
-        {
-            LOG_INFO("Y+ Disengaged");
-        }
-        HAL_GPIO_EXTI_IRQHandler(ySW_pos.pin);
+
         ySW_pos.Pin_state = yPin_p_state;
     }
 
-    if (ySW_neg.Pin_state != yPin_n_state && DEBOUNCE_OK(ySW_neg.lastDebounceTime, now))
+    // Y-
+    if (ySW_neg.Pin_state != yPin_n_state)
     {
-        ySW_neg.lastDebounceTime = now;
-        if (yPin_n_state)
-        {
-            motorY.isMoving = 0;
-            LOG_INFO("Y- Engaged");
-        }
-        else
-        {
-            LOG_INFO("Y- Disengaged");
-        }
         HAL_GPIO_EXTI_IRQHandler(ySW_neg.pin);
+
+        if (DEBOUNCE_OK(ySW_neg.lastDebounceTime, now))
+        {
+            ySW_neg.lastDebounceTime = now;
+            if (yPin_n_state)
+            {
+                motorY.isMoving = 0;
+                LOG_INFO("Y- Engaged");
+            }
+            else
+            {
+                LOG_INFO("Y- Disengaged");
+            }
+        }
+
         ySW_neg.Pin_state = yPin_n_state;
     }
 
-    if (zSW_pos.Pin_state != zPin_p_state && DEBOUNCE_OK(zSW_pos.lastDebounceTime, now))
+    // Z+
+    if (zSW_pos.Pin_state != zPin_p_state)
     {
-        zSW_pos.lastDebounceTime = now;
-        if (zPin_p_state)
-        {
-            motorZ.isMoving = 0;
-            LOG_INFO("Z+ Engaged");
-        }
-        else
-        {
-            LOG_INFO("Z+ Disengaged");
-        }
         HAL_GPIO_EXTI_IRQHandler(zSW_pos.pin);
+
+        if (DEBOUNCE_OK(zSW_pos.lastDebounceTime, now))
+        {
+            zSW_pos.lastDebounceTime = now;
+            if (zPin_p_state)
+            {
+                motorZ.isMoving = 0;
+                LOG_INFO("Z+ Engaged");
+            }
+            else
+            {
+                LOG_INFO("Z+ Disengaged");
+            }
+        }
+
         zSW_pos.Pin_state = zPin_p_state;
     }
 
-    if (zSW_neg.Pin_state != zPin_n_state && DEBOUNCE_OK(zSW_neg.lastDebounceTime, now))
+    // Z-
+    if (zSW_neg.Pin_state != zPin_n_state)
     {
-        zSW_neg.lastDebounceTime = now;
-        if (zPin_n_state)
-        {
-            motorZ.isMoving = 0;
-            LOG_INFO("Z- Engaged");
-        }
-        else
-        {
-            LOG_INFO("Z- Disengaged");
-        }
         HAL_GPIO_EXTI_IRQHandler(zSW_neg.pin);
+
+        if (DEBOUNCE_OK(zSW_neg.lastDebounceTime, now))
+        {
+            zSW_neg.lastDebounceTime = now;
+            if (zPin_n_state)
+            {
+                motorZ.isMoving = 0;
+                LOG_INFO("Z- Engaged");
+            }
+            else
+            {
+                LOG_INFO("Z- Disengaged");
+            }
+        }
+
         zSW_neg.Pin_state = zPin_n_state;
     }
 }
